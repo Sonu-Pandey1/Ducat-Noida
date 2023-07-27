2e741b05363309d641adaccc0ae979d45fcc221d
sonu <sonupandeybaldeo2003@gmail.com>
2023-07-27 23:15:20 +0530

    27/07/2023/23:15

---
+++ src/component/BatchesList.jsx
@@ -0,0 +1,14 @@
+
+
+export default function BatchesList() {
+    return (
+        <>
+            <ul className="list-group">
+                <li className="list-group-item">Course <span>Java</span></li>
+                <li className="list-group-item">Branch <span>South Ex.</span></li>
+                <li className="list-group-item">Date : 27-08-2023 <span>Time:</span></li>
+                <li className="list-group-item"><button>Get a Call Back</button></li>
+            </ul>
+        </>
+    )
+}
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,6 +5,7 @@ import { FaRegEnvelope } from "react-icons/fa"
 import { NavLink } from "react-router-dom"
 import CourseItems from "./CourseItems"
 import Carousel from "react-elastic-carousel"
+import BatchesList from "./BatchesList"
 
 export default function Home() {
   return (
@@ -55,38 +56,59 @@ export default function Home() {
             <CourseItems title={"Microsoft Dynamic"} img={"https://admin.ducatindia.com/courseCategory/1676710652215Microsoft%20Dyanamic.png"} />
             <CourseItems title={"React"} img={"https://icon2.cleanpng.com/20180629/gi/kisspng-react-angularjs-javascript-library-front-and-back-atom-5b35ea5bda9a42.9151947115302600598954.jpg"} />
             <CourseItems title={"Others"} img={"https://cdn-icons-png.flaticon.com/128/888/888878.png"} />
-      
+
           </div>
         </div>
-      </section> 
+      </section>
       {/* Popular Courses */}
       <section className="Popular-Courses">
-      <h3>Popular Courses</h3>
-      <div className="carsol  mt-4 pb-4 pt-4">
-      <Carousel className="styling-example " itemsToShow={3} enableAutoPlay={true} pagination={false} >
-  <div><CourseItems btn={true} title={"MERN STACK"} img={"https://admin.ducatindia.com/course/1686720666118Mernstack.jpg"}/></div>
-  <div><CourseItems btn={true} title={"WEB DESIGNING"} img={"https://admin.ducatindia.com/course/1676885368692web%20designing.jpg"} /></div>
-  <div><CourseItems btn={true} title={"ETHICAL HACKING v12"} img={"https://admin.ducatindia.com/course/1676886015025Ethical%20Hacking.jpg"}/></div>
-  <div><CourseItems btn={true} title={"PHOTOGRAPHY"} img={"https://png.pngtree.com/png-vector/20220502/ourmid/pngtree-blue-aperture-photography-logo-png-image_4561840.png"}/></div>
-  <div><CourseItems btn={true} title={"BEST PYTHON TRANING COURSE"} img={"https://admin.ducatindia.com/course/1677044014782Python%20%202.jpg"}/></div>
-  <div><CourseItems btn={true} title={"BEST ERP SAP TRANING INSITUTE"} img={"https://admin.ducatindia.com/course/1676885174856Erp.jpg"}/></div>
-  <div><CourseItems btn={true} title={"DIGITAL MARKETING"} img={"https://admin.ducatindia.com/course/1688465092051digital-marketing-course%20in%20Delhi.jpg"}/></div>
-  <div><CourseItems btn={true} title={"PYTHON FULL STACK DEVELOPER"} img={"https://admin.ducatindia.com/course/1686720747674PythonFullstack.jpg"}/></div>
-  <div><CourseItems btn={true} title={"JAVA EXPERT"} img={"https://admin.ducatindia.com/course/1677043956520java%20New.jpg"}/></div>
-  <div><CourseItems btn={true} title={"MEAN STACK"} img={"https://admin.ducatindia.com/course/1686720562373MEANSTACK1.jpg"}/></div>
-  <div><CourseItems btn={true} title={"JAVA FULL STACK DEVELOPER"} img={"https://admin.ducatindia.com/course/1677490908031JavaFullStack.jpg"}/></div>
-  <div><CourseItems btn={true} title={"FRONT END DEVELOPER"} img={"https://admin.ducatindia.com/course/1676886015025Ethical%20Hacking.jpg"}/></div>
-</Carousel> 
-      </div>
+        <h3>Popular Courses</h3>
+        <div className="carsol  mt-4 pb-4 pt-4">
+          <Carousel className="styling-example " itemsToShow={3} enableAutoPlay={true} pagination={false} >
+            <div><CourseItems btn={true} title={"MERN STACK"} img={"https://admin.ducatindia.com/course/1686720666118Mernstack.jpg"} /></div>
+            <div><CourseItems btn={true} title={"WEB DESIGNING"} img={"https://admin.ducatindia.com/course/1676885368692web%20designing.jpg"} /></div>
+            <div><CourseItems btn={true} title={"ETHICAL HACKING v12"} img={"https://admin.ducatindia.com/course/1676886015025Ethical%20Hacking.jpg"} /></div>
+            <div><CourseItems btn={true} title={"PHOTOGRAPHY"} img={"https://png.pngtree.com/png-vector/20220502/ourmid/pngtree-blue-aperture-photography-logo-png-image_4561840.png"} /></div>
+            <div><CourseItems btn={true} title={"BEST PYTHON TRANING COURSE"} img={"https://admin.ducatindia.com/course/1677044014782Python%20%202.jpg"} /></div>
+            <div><CourseItems btn={true} title={"BEST ERP SAP TRANING INSITUTE"} img={"https://admin.ducatindia.com/course/1676885174856Erp.jpg"} /></div>
+            <div><CourseItems btn={true} title={"DIGITAL MARKETING"} img={"https://admin.ducatindia.com/course/1688465092051digital-marketing-course%20in%20Delhi.jpg"} /></div>
+            <div><CourseItems btn={true} title={"PYTHON FULL STACK DEVELOPER"} img={"https://admin.ducatindia.com/course/1686720747674PythonFullstack.jpg"} /></div>
+            <div><CourseItems btn={true} title={"JAVA EXPERT"} img={"https://admin.ducatindia.com/course/1677043956520java%20New.jpg"} /></div>
+            <div><CourseItems btn={true} title={"MEAN STACK"} img={"https://admin.ducatindia.com/course/1686720562373MEANSTACK1.jpg"} /></div>
+            <div><CourseItems btn={true} title={"JAVA FULL STACK DEVELOPER"} img={"https://admin.ducatindia.com/course/1677490908031JavaFullStack.jpg"} /></div>
+            <div><CourseItems btn={true} title={"FRONT END DEVELOPER"} img={"https://admin.ducatindia.com/course/1676886015025Ethical%20Hacking.jpg"} /></div>
+          </Carousel>
+        </div>
       </section>
 
       {/* section Upcoming Batches */}
       <section className="Upcoming-Batches">
-      <h1>Upcoming Batches</h1>
-      <h5>Choose Your Nearest Location</h5>
-      <div className="ourBrancheslist">
-        
-      </div>
+        <h1>Upcoming Batches</h1>
+        <h5>Choose Your Nearest Location</h5>
+        <div className="ourBrancheslist">
+          <div className=" branchsTabs d-flex justify-content-between">
+            <button>Noida Sector 16</button>
+            <button>Noida Sector 63</button>
+            <button>Pitampura</button>
+            <button>South Ex..</button>
+            <button>Ghaziabad</button>
+            <button>Gurugram</button>
+          </div>
+          <div className="container">
+            <div className=" row batchesListMain">
+               <div className="col-4"> <BatchesList /></div>
+               <div className="col-4"> <BatchesList /></div>
+               <div className="col-4"> <BatchesList /></div>
+               <div className="col-4"> <BatchesList /></div>
+               <div className="col-4"> <BatchesList /></div>
+               <div className="col-4"> <BatchesList /></div>
+            </div>
+            <div className="btncontainer d-flex justify-content-center mt-5 ">
+              <button className="btn btn-outline-primary subbtn">Load More</button>
+            </div>
+          </div>
+
+        </div>
 
       </section>
     </>
