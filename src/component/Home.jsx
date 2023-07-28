d3a837faf19d004e4f97aff6d0db082d840a1166
sonu <sonupandeybaldeo2003@gmail.com>
2023-07-28 14:55:40 +0530

    28/07/23

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -145,106 +145,109 @@ export default function Home() {
         </div>
       </section>
       {/* footer */}
-      <section id="Contact-Us" class="section8parent">
-                <section class="section8">
-                    <div class="subsection8">
-                        <div class="footer">
-                            <div class="footerinnerdiv footerinnerdiv1">
+      <section id="Contact-Us" className="section8parent">
+                <section className="section8">
+                    <div className="subsection8">
+                        <div className="footer">
+                            <div className="footerinnerdiv footerinnerdiv1">
                                 <h1>Quick Link</h1>
-                                <ul class="footerul">
+                                <ul className="footerul">
                                     <a href="">
-                                        <li class="footerli1">About Us</li>
+                                        <li className="footerli1">About Us</li>
                                     </a>
                                     <a href="">
-                                        <li class="footerli1">Contact Us</li>
+                                        <li className="footerli1">Contact Us</li>
                                     </a>
                                     <a href="">
-                                        <li class="footerli1">Privacy Policy</li>
+                                        <li className="footerli1">Privacy Policy</li>
                                     </a>
                                     <a href="">
-                                        <li class="footerli1">Terms & Condition</li>
+                                        <li className="footerli1">Terms & Condition</li>
                                     </a>
                                     <a href="">
-                                        <li class="footerli1">FAQs & Help</li>
+                                        <li className="footerli1">FAQs & Help</li>
                                     </a>
                                 </ul>
                             </div>
-                            <div class="footerinnerdiv footerinnerdiv2">
+                            <div className="footerinnerdiv footerinnerdiv2">
                                 <h1>Contact</h1>
-                                <ul class="footerul2">
+                                <ul className="footerul2">
                                     <a href="">
-                                        <li class="footerli2">123 Street, New York, USA</li>
+                                        <li className="footerli2">123 Street, New York, USA</li>
                                     </a>
                                     <a href="">
-                                        <li class="footerli22">+012 345 67890</li>
+                                        <li className="footerli22">+012 345 67890</li>
                                     </a>
                                     <a href="">
-                                        <li class="footerli23">info@example.com</li>
+                                        <li className="footerli23">info@example.com</li>
                                     </a>
                                 </ul>
-                                <div class="footersocicalicons">
-                                    <a class="btn btn-social" href=""><i class="fab fa-twitter"></i></a>
-                                    <a class="btn btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                                    <a class="btn btn-social" href=""><i class="fab fa-youtube"></i></a>
-                                    <a class="btn btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                                <div className="footersocicalicons">
+                                    <a className="btn btn-social" href=""><i className="fab fa-twitter"></i></a>
+                                    <a className="btn btn-social" href=""><i className="fab fa-facebook-f"></i></a>
+                                    <a className="btn btn-social" href=""><i className="fab fa-youtube"></i></a>
+                                    <a className="btn btn-social" href=""><i className="fab fa-linkedin-in"></i></a>
                                 </div>
                             </div>
-                            <div class="footerinnerdiv footerinnerdiv3">
+                            <div className="footerinnerdiv footerinnerdiv3">
                                 <h1>Gallery</h1>
-                                <div class="footersection3div">
-                                    <div class="footersection3innerdiv1">
-                                        <div class="footersection3dblinnerdiv d1">
+                                <div className="footersection3div">
+                                    <div className="footersection3innerdiv1">
+                                        <div className="footersection3dblinnerdiv d1">
                                             <img
-                                                src="https://demo.htmlcodex.com/2151/elearning-html-template/img/course-1.jpg">
+                                                src="https://demo.htmlcodex.com/2151/elearning-html-template/img/course-1.jpg"/>
                                         </div>
-                                        <div class="footersection3dblinnerdiv d2">
+                                        <div className="footersection3dblinnerdiv d2">
                                             <img
-                                                src="https://demo.htmlcodex.com/2151/elearning-html-template/img/course-2.jpg">
+                                                src="https://demo.htmlcodex.com/2151/elearning-html-template/img/course-2.jpg"/>
                                         </div>
-                                        <div class="footersection3dblinnerdiv d3">
+                                        <div className="footersection3dblinnerdiv d3">
                                             <img
-                                                src="https://demo.htmlcodex.com/2151/elearning-html-template/img/course-3.jpg">
+                                                src="https://demo.htmlcodex.com/2151/elearning-html-template/img/course-3.jpg"/>
                                         </div>
                                     </div>
-                                    <div class="footersection3innerdiv2">
-                                        <div class="footersection3dblinnerdiv2 d4">
+                                    <div className="footersection3innerdiv2">
+                                        <div className="footersection3dblinnerdiv2 d4">
                                             <img
-                                                src="https://demo.htmlcodex.com/2151/elearning-html-template/img/course-2.jpg">
+                                                src="https://demo.htmlcodex.com/2151/elearning-html-template/img/course-2.jpg"/>
                                         </div>
-                                        <div class="footersection3dblinnerdiv2 d5">
+                                        <div className="footersection3dblinnerdiv2 d5">
                                             <img
-                                                src="https://demo.htmlcodex.com/2151/elearning-html-template/img/course-3.jpg">
+                                                src="https://demo.htmlcodex.com/2151/elearning-html-template/img/course-3.jpg"/>
                                         </div>
-                                        <div class="footersection3dblinnerdiv2 d6">
+                                        <div className="footersection3dblinnerdiv2 d6">
                                             <img
-                                                src="https://demo.htmlcodex.com/2151/elearning-html-template/img/course-1.jpg">
+                                                src="https://demo.htmlcodex.com/2151/elearning-html-template/img/course-1.jpg"/>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="footerinnerdiv footerinnerdiv4">
+                            <div className="footerinnerdiv footerinnerdiv4">
                                 <h1>Newsletter</h1>
-                                <p>Dolor amet sit justo amet elitr<br> clita ipsum elitr est.</p>
-                                <input type="email" class="email" placeholder="Your email" required>
+                                <p>Dolor amet sit justo amet elitr<br/> clita ipsum elitr est.</p>
+                                <div className="btnContainer d-flex">
+                                <input type="email" className="email" placeholder="Your email" required/>
                                 <button>SignUp</button>
-                                <!-- 84485 13439 Khushi -->
+                                </div>
+                              
                             </div>
                         </div>
                     </div>
-                    <div class="copyright">
-                        <div class="innerparentdiv">
-                            <p>© <a href="#"> MindedCoder </a>, All Right Reserved. Designed By <a href="#"> Sonu
+                    <div className="copyright">
+                        <div className="innerparentdiv">
+                            <p>© <a href="#"> Ducat</a>  All Right Reserved. Designed By <a href="#"> Sonu
                                     Pandey</a></p>
-                            <ul class="innerparentdivul">
-                                <li>Home</li>
-                                <li>Cookies</li>
-                                <li>Help</li>
-                                <li>FAQs</li>
+                            <ul className="innerparentdivul">
+                                <li><a href="">Home</a></li>
+                                <li><a href="">Cookies</a></li>
+                                <li><a href="">Help</a></li>
+                                <li><a href="">FAQs</a></li>
                             </ul>
                         </div>
                     </div>
                 </section>
             </section>
+     
     </>
   )
 }
